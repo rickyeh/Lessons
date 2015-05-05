bb9cb2410f0044eef1c45d9067b6de85ee6d854d
RickYeh <RickYeh@users.noreply.github.com>
2015-05-05 14:49:13 -0700

    Added some DIVs

--- practice/peerJS/hello.js
+++ practice/peerJS/hello.js
@@ -2,42 +2,52 @@ var $messageBox;
 var $yourID;
 var $sendID;
 var sendID;
+var sendText;
 
 $(document).ready(function() { 
     $messageBox = $('#messages');
     $yourID = $('#yourID');
     $sendID = $('#sendID');
+    $sendContent = $('#sendContent');
+    $sendButton = $('#sendButton');
+    $sendButton.click(send);
 });
 
+// Create new Peer Object
 var peer = new Peer({key: 'p4tiwn62dkt3ayvi'});
 
-$(document).ready(function() {
-
-    peer.on('open', function(id) {
-        console.log('My peer ID is: ' + id);
-        $yourID.append(id);
-    });
-
-    peer.on('connection', function(conn) {
-        console.log('Connection received!  SUCCESS!');
-        $messageBox.text('Connection received! Yay!')
-    });
-    
+peer.on('open', function(id) {
+    console.log('My peer ID is: ' + id);
+    $yourID.append(id);
 });
 
-function testFunction() {
+peer.on('connection', function(conn) {
+    console.log('Connection received!  SUCCESS!');
+    $messageBox.text('Connection received! Yay!');
+});
+
+// Send function called when the button is clicked.
+function send() {
     sendID = $sendID.text();
-    console.log(sendID);
+    sendText = $sendContent.text();
+    console.log('Sending message: ' + sendText + ' to ' + sendID);
+
+    $sendContent = $sendContent.text('');
 
     var conn = peer.connect(sendID);
 
     conn.on('open', function() {
-      // Receive messages
-      conn.on('data', function(data) {
-        console.log('Received', data);
-      });
+        // Receive messages
+        conn.on('data', function(data) {
+            console.log('Message Received');
+            console.log('Received', data);
+        });
 
-      // Send messages
-      conn.send('Hello!');
+        // Send messages
+        conn.send(sendText);
     });
+}
+
+function receive() {
+
 }
