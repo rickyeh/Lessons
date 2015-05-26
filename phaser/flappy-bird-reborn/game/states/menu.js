bcc2ea1516c9fca3f9cdbced5815f16876d30469
RickYeh <RickYeh@users.noreply.github.com>
2015-05-26 16:14:37 -0700

    Phaser Codevinsky tutorial

---
+++ phaser/flappy-bird-reborn/game/states/menu.js
@@ -0,0 +1,21 @@
+
+'use strict';
+function Menu() {}
+
+Menu.prototype = {
+  preload: function() {
+
+  },
+  create: function() {
+    this.background = this.game.add.sprite(0, 0, 'background');
+
+    // add the ground sprite as a tile
+    // and start scrolling in the negative x direction
+    this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
+    this.ground.autoScroll(-200, 0);
+  },
+  update: function() {
+  }
+};
+
+module.exports = Menu;
