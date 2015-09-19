d781ea97af8bdc41c564655a28181c29f58d6079
RickYeh <RickYeh@users.noreply.github.com>
2015-09-19 04:04:55 -0700

    Alphabet Soup - Function to return sorted string

---
+++ coderbyte/alphabetSoup.js
@@ -0,0 +1,17 @@
+function AlphabetSoup(str) { 
+
+    str = str.split('');
+    str.sort();
+
+    str = str.join('');
+  
+    return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+
+AlphabetSoup(readline();  
+                      
+
