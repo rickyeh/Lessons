0abf16e4afa6302f267998dce2ad561fe6c96944
RickYeh <RickYeh@users.noreply.github.com>
2015-09-22 19:02:32 -0700

    Learnyounode Modules

--- freeCodeCamp/nodeJS/program4.js
+++ freeCodeCamp/nodeJS/program4.js
@@ -1,7 +1,14 @@
-var fs = require('fs');
 var mymodule = require('./program4module.js');
 
 var filePath = process.argv[2];
 var filterExtension = process.argv[3];
 
-fs.readdir(filePath, mymodule.listDirectory);
+mymodule(filePath, filterExtension, function(err, list) {
+    if (err) {
+        return console.error('Error: ', err);
+    }
+    
+    for (var i=0; i<list.length; i++) {
+        console.log(list[i]);
+    }
+});
--- freeCodeCamp/nodeJS/program4module.js
+++ freeCodeCamp/nodeJS/program4module.js
@@ -1,16 +1,24 @@
-module.exports = function listDirectory(pathName, filterExtension, callback) {
+var fs = require('fs');
 
-}
+module.exports = function (path, extension, callback) {
+    fs.readdir(path, callbackFnc);
 
-function callbackFnc(err, list){
-    if (err) {
-        throw err;
-    }
-
-    for(var i = 0; i<list.length; i++){
-        if(list[i].indexOf('.' + filterExtension) > -1 ) {
-            console.log(list[i]);
+    function callbackFnc(err, list) {
+        if (err) {
+            callback(err, list);
+            return;
         }
+        
+        var filteredFiles = [];
+
+        for (var i = 0; i<list.length; i++) {
+            var fileName = list[i];
+            
+            if (fileName.indexOf('.' + extension) > -1 ) {
+                filteredFiles.push(fileName);
+            }
+        }
+        callback(null, filteredFiles);
     }
-}
+};
 
