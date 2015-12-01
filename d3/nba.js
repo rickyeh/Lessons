c906947541283bff45cf8db27513cda3d073479b
RickYeh <RickYeh@users.noreply.github.com>
2015-11-30 17:53:17 -0800

    Added free throws graph

--- d3/nba.js
+++ d3/nba.js
@@ -3,6 +3,7 @@ d3.csv('highScores.csv', type, function(error, data) {
     var width = 1000;
     var barHeight = 40;
     var height = (barHeight + 10) * data.length;
+    var padding = 50;
 
     var barChart = d3.select('#container')
         .append('svg:svg')
@@ -61,11 +62,85 @@ d3.csv('highScores.csv', type, function(error, data) {
         .attr('y', function(d, i) {
             return y(i);
         })
-        .attr('dx', '-3em')
-        .attr('dy', '1.5em')
+        .attr('dx', '-6em')
+        .attr('dy', '1.6em')
         .attr('text-anchor', 'right')
         .text(function(d) {
-            return d.PTS;
+            return d.PTS + ' PPG';
+        })
+        .attr('fill', 'white')
+        .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
+});
+
+d3.csv('freeThrows.csv', type, function(error, data) {
+
+    var width = 1000;
+    var barHeight = 40;
+    var height = (barHeight + 10) * data.length;
+    var padding = 50;
+
+    var barChart = d3.select('#container2')
+        .append('svg:svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('style', 'background-color: #EEE');
+
+    // X Scale
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) {
+            return d['FT%'];
+        })])
+        .rangeRound([0, width]);
+
+    // Y Scale
+    var y = d3.scale.linear()
+        .domain([0, data.length])
+        .range([0, height]);
+
+    barChart.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('svg:rect')
+        .attr('x', 0)
+        .attr('y', function(d, i) {
+            return y(i);
+        })
+        .attr('height', barHeight)
+        .attr('width', function(d) {
+            return x(d['FT%']);
+        })
+        .attr('fill', '#2d578b');
+
+    barChart.selectAll('textPlayer')
+        .data(data)
+        .enter()
+        .append('svg:text')
+        .attr('x', 0)
+        .attr('y', function(d, i) {
+            return y(i);
+        })
+        .attr('dx', '1em')
+        .attr('dy', '1.5em')
+        .attr('text-anchor', 'left')
+        .text(function(d) {
+            return d.Player;
+        })
+        .attr('fill', 'white')
+        .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
+
+    barChart.selectAll('textPts')
+        .data(data)
+        .enter()
+        .append('svg:text')
+        .attr('x', function(d) { return x(d['FT%']); }) 
+        .attr('y', function(d, i) {
+            return y(i);
+        })
+        .attr('dx', '-6em')
+        .attr('dy', '1.6em')
+        .attr('text-anchor', 'right')
+        .text(function(d) {
+            return d['FT%'] + ' FT%';
         })
         .attr('fill', 'white')
         .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
