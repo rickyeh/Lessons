b2028faf1466e8d1c3876be0e6be557b5cfab46c
RickYeh <RickYeh@users.noreply.github.com>
2016-05-24 12:04:32 -0700

    Adds parseJSON

---
+++ practice/parseJSON/test.js
@@ -0,0 +1,5 @@
+var test = '[{"nm":   "Edmund lronside", "cty": "United Kingdom", "hse": "House of Wessex", "yrs": "1016"}, {"nm": "Cnut", "cty": "United Kingdom", "hse": "House of Denmark", "yrs": "1016-1035"}, {"nm": "Harold I Harefoot", "cty": "United Kingdom", "hse": "House of Denmark", "yrs": "1035-1040"} ]';
+
+
+var foo = json_parse(test);
+console.log(foo);
