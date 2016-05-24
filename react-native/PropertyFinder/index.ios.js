dd34099a8e562c9306f19d715cf17c0fb5a32159
RickYeh <RickYeh@users.noreply.github.com>
2016-05-23 17:30:40 -0700

    Initial commit for property finder tutorial

---
+++ react-native/PropertyFinder/index.ios.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict'
+
+var React = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'PropertyFinder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
+
+
+
+
+
+
+
+// import React, {
+//   AppRegistry,
+//   Component,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// class PropertyFinder extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
